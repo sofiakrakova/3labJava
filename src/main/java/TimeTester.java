9f9a149df0b4369a62a90b1285e9b2cafb82af5e
sofiakrakova <sofiakrakova84@gmail.com>
2021-04-12 23:50:45 +0300

    Add javadoc

--- src/main/java/TimeTester.java
+++ src/main/java/TimeTester.java
@@ -1,6 +1,16 @@
 import java.util.List;
+/**
+ * Class for testing the productivity of ArrayList and LinkedList
+ */
 public class TimeTester
  {
+     /**
+      * Method for testing the speed of adding items in list
+      * @param list - list to add items
+      * @param <T> - type for list items
+      * @param size - count of items for adding
+      * @return time of adding items
+      */
      public static <T> long addList(List <T>list, int size)
      {
         long start = System.nanoTime();
@@ -11,6 +21,13 @@ public class TimeTester
         return  System.nanoTime() - start;
 
     }
+     /**
+      * Method for testing the speed of getting items from the list
+      * @param list - list of items to get
+      * @param <T> - type for list items
+      * @param size - count of items for getting from the list
+      * @return time of getting items
+      */
      public static <T> long getList(List <T>list, int size)
      {
          long start = System.nanoTime();
@@ -19,6 +36,13 @@ public class TimeTester
         }
          return  System.nanoTime() - start;
     }
+     /**
+      * Method for testing the speed of removing items from the list
+      * @param list - list of items to remove
+      * @param <T> - type for list items
+      * @param size - count of items for removing from the list
+      * @return time of removing items
+      */
 
      public static <T> long removeList(List <T>list, int size)
      {
